d326e7421762695bfb8ec802977a96421c056fb9
TuffStuff2K7 <atulhegde.2k7@gmail.com>
2021-02-05 20:45:10 +0530

    Indentation and stuff

--- sketch.js
+++ sketch.js
@@ -4,32 +4,35 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg, platform;
 
 function preload() {
+    
     backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
+
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(100,300,500,200);
+    ground = new Ground(600, height, 1200, 20);
+    platform = new Ground(100, 300, 500, 200);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box1 = new Box(700, 320, 70, 70);
+    box2 = new Box(920, 320, 70, 70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810, 260 ,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    box3 = new Box(700, 240, 70, 70);
+    box4 = new Box(920, 240, 70, 70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 =  new Log(810, 180, 300, PI/2);
 
-    box5 = new Box(810,160,70,70);
+    box5 = new Box(810, 160, 70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
@@ -60,4 +63,10 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    nothing();
+}
+
+function nothing(){
+    rect(20,20,60000,60000);
 }
